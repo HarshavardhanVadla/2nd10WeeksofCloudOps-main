49a8284aa1865fa597304e59b68305421c46ae8d
HarshavardhanVadla <harshavardhanvadla6@gmail.com>
2024-09-26 12:26:29 -0400

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "projh"
+    bucket = "book-project-h"
     key    = "eks/terraform.tfstate"
-    region = "sa-east-1"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
